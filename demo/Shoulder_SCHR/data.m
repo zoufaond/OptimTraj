ea7b4c122ac5ecfa8768f8dc9bce299e97aaa7fa
zoufaond <zoufik.o@seznam.cz>
2024-03-26 10:47:46 +0100

    model correct

--- demo/Shoulder_SCHR/data.m
+++ demo/Shoulder_SCHR/data.m
@@ -1,8 +1,8 @@
 % clear all
-c = 0.01;
+c = 1;
 x0 = [0,2.8,0,0]*0;
-ms = 1;
-mh = 1;
+ms = 0.5;
+mh = 2;
 Is = 0.02;
 Ih = 0.02;
 GHsx = 0.075;
@@ -15,10 +15,10 @@ coords = [GHsx GHsy GHhx GHhy];
 origin = [1,2];
 insertion = [3,3];
 O_pos = [-0.01,0.01;0,-0.02];
-I_pos = [0,0.01;0,-0.01];
-l0m = [0.4,0.3]*2;
-force = [1,1]*100;
-akt = [1,1]*10;
+I_pos = [0,0.12;0,-0.01];
+l0m = [0.095,0.9,0.135,0.6];
+force = [100,100,2000,100]*(-1);
+akt = [1,0,0,0]*1;
 % x1 = 0; dx1 = 0; ddx1 = 0; x2 = 0; dx2 = 0; ddx2 = 0; akt = [1,1]*0;
 % muscle_force_direction(1,origin(1),insertion(1),O_pos(1,:),I_pos(1,:),1,1,x1,x2,GHsx,GHsy,GHhx,GHhy)
 % reactions(x1,dx1,ddx1,x2,dx2,ddx2,GHsx,GHsy,GHhx,GHhy,g,mh,origin,insertion,O_pos,I_pos,l0m,force,akt)
--- demo/Shoulder_SCHR/eoms_derivation.m
+++ demo/Shoulder_SCHR/eoms_derivation.m
@@ -1,24 +1,32 @@
 clear all;clc
 syms ms mh g c Is Ih GHsx GHsy GHhx GHhy qq1 qq2 uu1 uu2 ddq1 ddq2 t real
-F_iso = sym('F_iso',[1 2]);
-l0m = sym('l0m', [1 2]);
-akt = sym('akt',[1 2]);
+F_iso = sym('F_iso',[1 4]);
+l0m = sym('l0m', [1 4]);
+akt = sym('akt',[1 4]);
 q = [qq1 qq2 uu1 uu2];
 q_ID = [qq1 qq2 uu1 uu2 ddq1 ddq2];
 koef = [ms mh Is Ih g c];
 coords = [GHsx GHsy GHhx GHhy];
 mus1_origin = [-0.01,0.01];
-mus1_insertion = [0,0.01];
+mus1_insertion = [0,0.12];
 mus2_origin = [0,-0.02];
 mus2_insertion = [0,-0.01];
+mus3_origin = [-0.08,0.08];
+mus3_insertion = [0.05,0.02];
+mus4_origin = [-0.12,-0.02];
+mus4_insertion = [-0.08,-0.15];
 
 muscle1_len = muscle_length('Thorax','Humerus',mus1_origin,mus1_insertion,[qq1,qq2]);
 muscle2_len = muscle_length('Scapula','Humerus',mus2_origin,mus2_insertion,[qq1,qq2]);
+muscle3_len = muscle_length('Thorax','Scapula',mus3_origin,mus3_insertion,[qq1,qq2]);
+muscle4_len = muscle_length('Thorax','Scapula',mus4_origin,mus4_insertion,[qq1,qq2]);
 muscle1_force = muscle_force(muscle1_len,F_iso(1), akt(1), l0m(1));
 muscle2_force = muscle_force(muscle2_len,F_iso(2), akt(2), l0m(2));
-
-lengths = [muscle1_len,muscle2_len];
-forces = [muscle1_force,muscle2_force]';
+muscle3_force = muscle_force(muscle3_len,F_iso(3), akt(3), l0m(3));
+muscle4_force = muscle_force(muscle4_len,F_iso(4), akt(4), l0m(4));
+% 
+lengths = [muscle1_len,muscle2_len,muscle3_len,muscle4_len];
+forces = [muscle1_force,muscle2_force,muscle3_force,muscle4_force]';
 fe = [zeros(2,1);jacobian(lengths,[qq1,qq2])'*forces];
 mm = [1 0 0 0; 0 1 0 0; 0 0 Ih + Is + mh.*(GHhx.^2 - GHhx.*(GHsx.*cos(qq2) + GHsy.*sin(qq2)) + GHhy.^2 + GHhy.*(GHsx.*sin(qq2) - GHsy.*cos(qq2)) + GHsx.^2 + GHsx.*(-GHhx.*cos(qq2) + GHhy.*sin(qq2)) + GHsy.^2 - GHsy.*(GHhx.*sin(qq2) + GHhy.*cos(qq2))) Ih + mh.*(GHhx.^2 + GHhy.^2 + GHsx.*(-GHhx.*cos(qq2) + GHhy.*sin(qq2)) - GHsy.*(GHhx.*sin(qq2) + GHhy.*cos(qq2))); 0 0 Ih + mh.*(GHhx.^2 - GHhx.*(GHsx.*cos(qq2) + GHsy.*sin(qq2)) + GHhy.^2 + GHhy.*(GHsx.*sin(qq2) - GHsy.*cos(qq2))) Ih + mh.*(GHhx.^2 + GHhy.^2)];
 fo = [uu1; uu2; GHhx.*g.*mh.*(-sin(qq1).*sin(qq2) + cos(qq1).*cos(qq2)) - GHhx.*mh.*(uu1 + uu2).^2.*(GHsx.*sin(qq2) - GHsy.*cos(qq2)) - GHhy.*g.*mh.*(sin(qq1).*cos(qq2) + sin(qq2).*cos(qq1)) - GHhy.*mh.*(uu1 + uu2).^2.*(GHsx.*cos(qq2) + GHsy.*sin(qq2)) - GHsx.*g.*mh.*cos(qq1) + GHsx.*mh.*uu1.^2.*(GHhx.*sin(qq2) + GHhy.*cos(qq2)) + GHsy.*g.*mh.*sin(qq1) + GHsy.*mh.*uu1.^2.*(-GHhx.*cos(qq2) + GHhy.*sin(qq2)) - c.*uu1; GHhx.*g.*mh.*(-sin(qq1).*sin(qq2) + cos(qq1).*cos(qq2)) - GHhy.*g.*mh.*(sin(qq1).*cos(qq2) + sin(qq2).*cos(qq1)) + GHsx.*mh.*uu1.^2.*(GHhx.*sin(qq2) + GHhy.*cos(qq2)) + GHsy.*mh.*uu1.^2.*(-GHhx.*cos(qq2) + GHhy.*sin(qq2)) - c.*uu2];
@@ -26,8 +34,11 @@ dq = mm\(fo+fe);
 torque = mm(3:end,3:end)*[ddq1;ddq2]-fo(3:end)-fe(3:end);
 
 
-matlabFunction(dq,'file','ODE_sim','vars',{t,q',koef,coords,F_iso,l0m,akt})
-matlabFunction(torque,'file','Inverse_dyn','vars',{t,q_ID',koef,coords,F_iso,l0m,akt})
+matlabFunction(mm,'file','MM','vars',{t,q',koef,coords,F_iso,l0m,akt})
+matlabFunction(fo,'file','FO','vars',{t,q',koef,coords,F_iso,l0m,akt})
+matlabFunction(fe,'file','FE','vars',{t,q',koef,coords,F_iso,l0m,akt})
+% matlabFunction(dq,'file','ODE_sim','vars',{t,q',koef,coords,F_iso,l0m,akt})
+% matlabFunction(torque,'file','Inverse_dyn','vars',{t,q_ID',koef,coords,F_iso,l0m,akt})
 
 function force = muscle_force(length, F_iso, akt, l0m)
     f_gauss = 0.25;
