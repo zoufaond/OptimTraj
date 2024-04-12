97e5e1cabccfedc8e3e1132456b93e0e6a9dc3dd
zoufaond <zoufik.o@seznam.cz>
2024-04-12 08:42:30 +0200

    wrapping OK

--- demo/Shoulder_SCHR/eoms_derivation.m
+++ demo/Shoulder_SCHR/eoms_derivation.m
@@ -32,8 +32,8 @@ muscle5_force = muscle_force(muscle5_len,F_iso(5), akt(5), l0m(5));
 lengths = [muscle1_len,muscle2_len,muscle3_len,muscle4_len,muscle5_len];
 forces = [muscle1_force,muscle2_force,muscle3_force,muscle4_force,muscle5_force]';
 % jacob = jacobian(lengths,[qq1,qq2])';
-jacob = jacobian(lengths,[qq1,qq2])';
-jacob(2,2) = -0.05;
+jacob = simplify(jacobian(lengths,[qq1,qq2])');
+% jacob(2,2) = -0.05;
 fe = [zeros(2,1);jacob*forces];
 mm = [1 0 0 0; 0 1 0 0; 0 0 Ih + Is + mh.*(GHhx.^2 - GHhx.*(GHsx.*cos(qq2) + GHsy.*sin(qq2)) + GHhy.^2 + GHhy.*(GHsx.*sin(qq2) - GHsy.*cos(qq2)) + GHsx.^2 + GHsx.*(-GHhx.*cos(qq2) + GHhy.*sin(qq2)) + GHsy.^2 - GHsy.*(GHhx.*sin(qq2) + GHhy.*cos(qq2))) Ih + mh.*(GHhx.^2 + GHhy.^2 + GHsx.*(-GHhx.*cos(qq2) + GHhy.*sin(qq2)) - GHsy.*(GHhx.*sin(qq2) + GHhy.*cos(qq2))); 0 0 Ih + mh.*(GHhx.^2 - GHhx.*(GHsx.*cos(qq2) + GHsy.*sin(qq2)) + GHhy.^2 + GHhy.*(GHsx.*sin(qq2) - GHsy.*cos(qq2))) Ih + mh.*(GHhx.^2 + GHhy.^2)];
 fo = [uu1; uu2; GHhx.*g.*mh.*(-sin(qq1).*sin(qq2) + cos(qq1).*cos(qq2)) - GHhx.*mh.*(uu1 + uu2).^2.*(GHsx.*sin(qq2) - GHsy.*cos(qq2)) - GHhy.*g.*mh.*(sin(qq1).*cos(qq2) + sin(qq2).*cos(qq1)) - GHhy.*mh.*(uu1 + uu2).^2.*(GHsx.*cos(qq2) + GHsy.*sin(qq2)) - GHsx.*g.*mh.*cos(qq1) + GHsx.*mh.*uu1.^2.*(GHhx.*sin(qq2) + GHhy.*cos(qq2)) + GHsy.*g.*mh.*sin(qq1) + GHsy.*mh.*uu1.^2.*(-GHhx.*cos(qq2) + GHhy.*sin(qq2)) - c.*uu1; GHhx.*g.*mh.*(-sin(qq1).*sin(qq2) + cos(qq1).*cos(qq2)) - GHhy.*g.*mh.*(sin(qq1).*cos(qq2) + sin(qq2).*cos(qq1)) + GHsx.*mh.*uu1.^2.*(GHhx.*sin(qq2) + GHhy.*cos(qq2)) + GHsy.*mh.*uu1.^2.*(-GHhx.*cos(qq2) + GHhy.*sin(qq2)) - c.*uu2];
@@ -112,7 +112,7 @@ function len = muscle_length_wrap(origin,insertion,centre,radius)
     O_pos = wrap_point(origin,centre,radius,-1);
     O_len = vec_dist(O_pos,origin);
     I_len = vec_dist(I_pos,insertion);
-    len =wrap_len+wrap_len+O_len+I_len;
+    len = wrap_len+O_len+I_len;
 end
 
 function len = vec_dist(O,I)
--- demo/Shoulder_SCHR/obtaceni.m
+++ demo/Shoulder_SCHR/obtaceni.m
@@ -1,21 +1,32 @@
 clear all
-centre = [1,2,0];
-orig = [-0.5,3,0];
-insert = [2,0,0];
+syms phi real
+centre = [0,1,0];
+orig = [-2,2,0];
+insertion = [4,2,0];
+insert = I_pos(phi,insertion,[0,0,0])';
 radius = 1;
 
-pos1 = wrap_point(orig, centre, radius,-1);
-pos2 = wrap_point(insert, centre, radius,1);
-angle = wrap_angle(orig,insert,centre,radius)*180/pi
-length = muscle_length_wrap(orig,insert,centre,radius)
+length = muscle_length_wrap(orig,insert,centre,radius);
+assume(phi,'real')
+length = (length);
+len = (matlabFunction(length));
+moment_arm = (diff(len,phi));
+mom_arm = matlabFunction(moment_arm);
+% phi_vec = linspace(0,0,100);
+phi_vec = 0;
+figure
+plot(phi_vec,mom_arm(phi_vec),'o')
+figure
+plot(phi_vec,len(phi_vec),'o')
 
-% vectoangle = vecangle360(x_cyl,x_ax,side);
-figure;
-hold on
-plot (orig(1),orig(2),'x',pos1(1),pos1(2),'o');
-plot (insert(1),insert(2),'x',pos2(1),pos2(2),'o');
-circle(centre,radius);
-daspect([1,1,1])
+
+% % vectoangle = vecangle360(x_cyl,x_ax,side);
+% figure;
+% hold on
+% plot (orig(1),orig(2),'x',pos1(1),pos1(2),'o');
+% plot (insert(1),insert(2),'x',pos2(1),pos2(2),'o');
+% circle(centre,radius);
+% daspect([1,1,1])
 
 function len = muscle_length_wrap(origin,insertion,centre,radius)
     wrap_len = wrap_angle(origin,insertion,centre,radius)*radius;
@@ -78,6 +89,10 @@ end
 
 function r = position(x,y)
     r = [x;y;1];
+end
+
+function pos = I_pos(phi,coords,centre)
+    pos = T_x(centre(1))*T_y(centre(2))*R_z(phi)*position(coords(1),coords(2));
 end
 
 function h = circle(pos,r)
